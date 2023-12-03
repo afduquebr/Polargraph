5690463efe6fad156fde83f19891834961ab125b
Andrés Duque Bran <afduquebr@unal.edu.co>
2023-12-03 12:49:12 +0100

    Bugs fixed tested in the simulator

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -2,8 +2,8 @@
 #include "simulator.h"
 using namespace std;
 
-float init_x = 50.0; // Define init position in x
-float init_y = 50.0; // Define init position in y
+float init_x = 0.0; // Define init position in x
+float init_y = 0.0; // Define init position in y
 
 int state = 0; // Stage in which pointer is moving
 bool change = 0; // True if state must change
@@ -16,25 +16,39 @@ int main() {
 
   cout << "x: " << pointer.position.x << " y: " << pointer.position.y << "\n";  
 
+  cout << "Go to initial position! \n";
+
   while (state == 0) {
     cout << "x: " << pointer.position.x << " y: " << pointer.position.y << "\n";  
-    change = pointer.moveInX(50.20);
+    change = pointer.initPosition(0.0, 0.0);
     if (change == 1) {
       state += 1;
     } 
   }
 
-  cout << " Now we do a square! \n";
+  cout << "Now we do a square! \n";
   
   int sq_state = 0;
   while (state == 1) {
-    change = pointer.square(55.0, 55.0, 5.0, sq_state);
+    change = pointer.square(0.0, 0.0, 70.0, sq_state);
     if (change == 1) {
       state += 1;
     } 
     cout << "x: " << pointer.position.x << " y: " << pointer.position.y << "\n";  
   }
 
+  cout << "We go back to rest position! \n";
+
+  while (state == 2) {
+      change = pointer.restPosition();
+      if (change == 1) {
+        state += 1;
+      } 
+      cout << "x: " << pointer.position.x << " y: " << pointer.position.y << "\n";  
+    }
+
   return 0;
 }
+
+
 
